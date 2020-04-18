8aec2fd0ff57f5354fb11e9361a3af31e45e0fca
Joe <joeythekid031@gmail.com>
2020-04-18 13:58:20 -0500

    added code for contitional

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var startBtn = $('#start-btn')
 highScoreModal =  $('.modal')
 $(".is-info").click(function(){
@@ -6,3 +7,19 @@ $(".is-info").click(function(){
 $(".exit-modal").click(function(){
     highScoreModal.removeClass("is-active")
 })
+=======
+var score= "good"
+var simpsonsurl = "http://api.giphy.com/v1/gifs/search?q="+ score +"+simpsonsling&api_key=Rpk0iiKiEri4SFv9B1aOmJlCb0UllXnN"
+
+
+$.ajax({
+    url: simpsonsurl,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function(response) {
+      console.log(response)
+      console.log(response[0].character)
+      Rpk0iiKiEri4SFv9B1aOmJlCb0UllXnN
+    });
+>>>>>>> Stashed changes
