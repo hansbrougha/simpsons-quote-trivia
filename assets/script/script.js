98eade60796b3f002f1548fb13e351fea585715d
Jacob Walton <jawalton6616@gmail.com>
2020-04-18 13:55:09 -0500

    removed start div on click

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,17 @@
 var startBtn = $('#start-btn')
+
+//removes start page div
+startBtn.click(function(){
+$('#start-page').attr("style","display: none")
+})
+
+//displays high score modal
 highScoreModal =  $('.modal')
 $(".is-info").click(function(){
     highScoreModal.addClass("is-active")
 })
+
+//removes high score modal
 $(".exit-modal").click(function(){
     highScoreModal.removeClass("is-active")
 })
