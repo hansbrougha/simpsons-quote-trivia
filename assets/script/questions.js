c2136c2a410f687ea7924ad5f68bef4466fa6c61
Lance Page <6ftdotexe@users.noreply.github.com>
2020-04-18 13:58:59 -0500

    reformated to pull 10 quotes at once and build a unique array of those pulled

--- assets/script/questions.js
+++ assets/script/questions.js
@@ -1,26 +1,44 @@
 // Do until 4 unique characters and quotes
 
+    var characters = [""];
+    var quotes = [""];
 
+//for(var i = 0; i < 5; i++){
+//do{
 // pull quote
 
-var quote = "https://thesimpsonsquoteapi.glitch.me/quotes";
-
 $.ajax({
-    url: quote,
+    url: "https://thesimpsonsquoteapi.glitch.me/quotes?count=25",
     method: "GET"
   }).then(function(response) {
-    console.log(quote);
-    console.log(response);
-    console.log(response[0].character);
-    console.log(response[0].quote);
-  });
+        for(var z = 0; z <10; z++){
+            console.log(response[z].character);
+            var speaker = response[z].character;
+            console.log(response[z].quote);
+            var quotetext = response[z].quote;
+        
+            // check unique character and quote
 
-// check unique character and quote
+            for(var i = 0; i < 10; i++){    
+                if(quotes.indexOf(quotetext) === -1){
+                    if(characters.indexOf(speaker) === -1){
+                        characters.push(response[z].character);
+                        quotes.push(response[z].quote);
+                    }
+                }
+            }
+        }
+    console.log(quotes);
+    console.log(characters);
 
+    // Display character on button
 
-// Display character on button
+    
 
+    // Display character on button randomly
 
-// Display character on button randomly
+});
+//}
+//while(characters.length < 5);
 
-
+// totalPoints++ everytime correct awnser
