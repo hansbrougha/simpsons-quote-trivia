1eb79470e6266b401e551192df9d91cf5a8b9a83
Lance Page <6ftdotexe@users.noreply.github.com>
2020-04-25 12:26:51 -0500

    black bar still shows but functionally works with duplicates still

--- assets/script/questions.js
+++ assets/script/questions.js
@@ -1,158 +1,155 @@
-$(document).ready(function(){
-// Do until 4 unique characters and quotes
+// // Do until 4 unique characters and quotes
 
-var totalPoints=0;
+// var characters = JSON.parse(window.localStorage.getItem("characters")) || [];
+// var quotes = JSON.parse(window.localStorage.getItem("quotes")) || [];
 
-var characters = JSON.parse(window.localStorage.getItem("characters")) || [""];
-var quotes = JSON.parse(window.localStorage.getItem("quotes")) || [""];
+// console.log(quotes);
+// console.log(characters);
 
-var currentQuestionIndex = 1;
-//console.log(currentQuestionIndex);
+// $.ajax({
+// url: "https://thesimpsonsquoteapi.glitch.me/quotes?count=25",
+// method: "GET"
+// }).then(function(response) {
+//     for(var z = 0; z <10; z++){
+//         console.log(response[z].character);
+//         var speaker = response[z].character;
+//         var quotetext = response[z].quote;
 
-//console.log(quotes);
-//console.log(characters);
+//         // check unique character and quote
 
-for(var i = 0; i < 6; i++){
+//         for(var i = 0; i < 10; i++){    
+//             if(quotes.indexOf(quotetext) === -1){
+//                 if(characters.indexOf(speaker) === -1){
+//                     characters.push(response[z].character);
+//                     quotes.push(response[z].quote);
+//                 }
+//             }
+//         }
+//     }
+// console.log(quotes);
+// console.log(characters);
+
+
+// window.localStorage.setItem("characters", JSON.stringify(characters));
+// window.localStorage.setItem("quotes", JSON.stringify(quotes));
+
+// // Display character on button
+// for(var i = 0; i<5; i++){
+//     var buttons = "#button-" + JSON.stringify(i);
+//     console.log(buttons)
+//     $(buttons).text(characters[characters.length-i]);
+// }
+
+// // Display Quote at top
+
+// $("#quote-here").text(quotes[quotes.length-3]);
+
+// // Display character on button randomly
+
+
+// });
+
+// //}
+// //while(characters.length < 5);
+
+// // totalPoints++ everytime correct awnser
+var questionIndex = 0
+var correctAnswer = []
+var totalPoints = 0 
+
+$("#start-btn").on("click", function () {
+    displayquestion()
+    questionIndex = 0
+
+})
+
+$(".answer").on("click", function () {
+    var clicked = $(this).val()
+    console.log(clicked)
+    var acutalName = correctAnswer[questionIndex]
+    console.log(acutalName)
+    console.log(questionIndex)
+    console.log(correctAnswer)
+    if (clicked == acutalName) {
+        console.log("correct")
+        totalPoints++;
+        displayquestion()
+    } else {
+        console.log("wrong")
+        displayquestion()
+    }
+    questionIndex++;
+    if (questionIndex == 4) {
+        $("#user-input-modal").attr("class", "is-active")
+    }
+})
+function displayquestion() {
+
+
+
     $.ajax({
-    url: "https://thesimpsonsquoteapi.glitch.me/quotes?count=10",
-    method: "GET"
-    }).then(function(response) {
-        for(var z = 0; z <25; z++){
-            //console.log(response[z].character);
-            var speaker = response[z].character;
-            var quotetext = response[z].quote;
+        url: "https://thesimpsonsquoteapi.glitch.me/quotes?count=25",
+        method: "GET"
+    }).then(function (response) {
+        // console.log(response)
+        var randomNum = Math.floor(Math.random() * 9)
 
-            //console.log(speaker);
-        
-            // check unique character and quote
 
-            for(var i = 0; i < 11; i++){    
-                if(quotes.indexOf(quotetext) === -1){
-                    if(characters.indexOf(speaker) === -1){
-                        characters.push(response[z].character);
-                        quotes.push(response[z].quote);
-                    }
-                }
+        var quoteResponse = response[randomNum].quote
+        var quoteChar = response[randomNum].character
+        console.log(quoteChar)
+        correctAnswer.push(quoteChar);
+       
+        $("#quote-here").text(quoteResponse)
+        for (let i = 1; i < 6; i++) {
+            var qnum = Math.floor(Math.random() * 9);
+            var returnedName = response[qnum].character
+            var oldI = i - 1
+            var btn1val = $("#button-1").val()
+            var btn2val = $("#button-2").val()
+            var btn3val = $("#button-3").val()
+            var btn4val = $("#button-4").val()
+
+            
+            // will sometimes not place an answer on a button and still have duplicates 
+            if (returnedName === btn1val || returnedName === btn2val || returnedName === btn3val || returnedName === btn4val) {
+                $("#button-" + oldI).text(response[(qnum + 1)].character).attr("value", response[qnum + 1].character);
+                // $("#button-" + oldI).text("dup").val(response[qnum + 1].character);
+                var test = $("#button-" + i).val();
+                // console.log("value dup test", test)
+            } else {
+                $("#button-" + i).text(response[qnum].character).attr("value", response[qnum].character);
+
+                // var elsetest = $("#button-" + i).val();
+                // if (quoteChar !== btn1val || quoteChar !== btn2val || quoteChar !== btn3val || quoteChar !== btn4val) {
+                //     $("#button-" + correctNum).text(quoteChar);
+                //     console.log("changed to correct", quoteChar)
+                // }
+                
+
             }
+
+
+
+        };
+        // console.log(correctAnswer[0])
+        // console.log("btn1", btn1val);
+        // console.log("btn2", btn2val)
+        // console.log("btn3", btn3val)
+        // console.log("btn4", btn4val)
+
+
+        var correctNum = Math.floor(Math.random() * 4);
+        // sometimes will run even though the answer is already a value of a button
+        if (quoteChar !== btn1val || quoteChar !== btn2val || quoteChar !== btn3val || quoteChar !== btn4val) {
+            $("#button-" + correctNum).text(quoteChar).attr("value", quoteChar);
+            // console.log("changed to correct", quoteChar)
         }
 
-//console.log(quotes);
-//console.log(characters);
 
-window.localStorage.setItem("characters", JSON.stringify(characters));
-window.localStorage.setItem("quotes", JSON.stringify(quotes));
+        // var correctNum = Math.floor(Math.random()*4)
 
-
-// Display character on button
-//for(var i = 0; i<5; i++){
-    //var buttons = "#button-" + JSON.stringify(i);
-   // console.log(buttons)
-    //$(buttons).text(characters[characters.length-i]);
-//}
-
-// Display Quote at top
-
-//$("#quote-here").text(quotes[quotes.length-3]);
-
-// Display character on button randomly
+        // $("#button-" + correctNum).text(quoteChar);
 
     });
-}
-
-console.log("quotes", quotes);
-console.log("characters", characters);
-
-
-//}
-
-  //while(characters.length < 5);
-  //console.log(Math.floor(Math.random()*4))
-  // totalPoints++ everytime correct awnser
-
-  function getQuestion(){
-    var currentQuestion = quotes[currentQuestionIndex];
-    //console.log(currentQuestion);
-    console.log(currentQuestionIndex);
-    // change quesition text
-
-    var random = Math.floor(Math.random()*4)+1;
-    console.log("random", random);
-
-    // Get correct answer
-    var correct = characters[currentQuestionIndex+ random];
-    console.log("correct", correct);
-
-    $("#quote-here").text(quotes[currentQuestionIndex + random]);
-    console.log("quote", quotes[currentQuestionIndex + random])
-
-    // Display correct answer
-    // display options per question
-
-    for(var i = 1; i<5; i++){
-        var buttons = "#button-" + JSON.stringify(i);
-        //console.log(buttons)
-        $(buttons).text(characters[currentQuestionIndex*i]);
-        //console.log(characters[4*currentQuestionIndex+i]);
-        //console.log("button text", characters[(currentQuestionIndex*i)]);
-        $(buttons).val(characters[currentQuestionIndex*i]);
-        
-        //console.log("$(buttons).val()", $(buttons).val());
-    
-    }
-
-    check(correct, buttons, random);
-    console.log("#button-1", $("#button-1").val());
-    console.log("#button-2", $("#button-2").val());
-    console.log("#button-3", $("#button-3").val());
-    console.log("#button-4", $("#button-4").val());
-
-    $(".button").click(function(){questionClick(correct, buttons)});
-  }
-
-  
-
-    function check(correct, buttons, random){
-        // if a buttons text is equal to correct do not change other buttons to correct
-  
-        if($("#button-1").val() !== correct || $("#button-2").val() !== correct || $("#button-3").val() !== correct || $("#button-4").val() !== correct){
-            var buttons = "#button-" + random;
-            $(buttons).text(correct);
-            $(buttons).val(correct);
-            // why does this change no matter if correct is already on
-        }
-    }
-
-  function questionClick(correct, buttons){
-      //console.log(currentQuestionIndex);
-    var btn = $(buttons).val();
-    console.log("btn click", btn);
-      if(btn !== correct){
-          //incorrect;
-          var wrong=0;
-          wrong=wrong+1;
-      }
-      else{
-          //correct
-          totalPoints=totalPoints+1;
-          //console.log("points", totalPoints);
-      }
-      console.log("points", totalPoints);
-      //console.log("correct",correct);
-      currentQuestionIndex=currentQuestionIndex+1;
-      console.log("# Q", currentQuestionIndex);
-
-      if (currentQuestionIndex > 4) {
-        quizEnd();
-      } else {
-        getQuestion();
-      }
-  }
-
-  function quizEnd(){
-    $("#user-input-modal").attr("class", "is-active")
-  }
-
-
-$("#start-btn").click(function(){getQuestion()});
-
-});
+};
